40cb000357db3bb32b0741d67e99a22b8a876341
AdrianZarate <adrian.1478.rz@gmail.com>
2023-11-25 18:59:03 -0500

    arreglando el mierdero

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
-<?php
-include './extras/conexion.php';
-include './extras/consultas.php';
-
-$pizzas = obtenerPizzas();
-
-
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 
@@ -54,6 +42,143 @@ $pizzas = obtenerPizzas();
     </header>
     <!-- Sección de encabezado termina aquí -->
 
+    <!-- Sección de cuenta de usuario comienza aquí -->
+    <div class="user-account">
+        <section class="container">
+            <div id="close-account"><span>Cerrar</span></div>
+            <div class="user">
+                <p><span>No has iniciado sesión.</span></p>
+            </div>
+            <div class="display-orders">
+                <p>Pizza 01 <span>( $3/ - x 2 )</span></p>
+                <p>Pizza 02 <span>( $2/ - x 1 )</span></p>
+                <p>Pizza 03 <span>( $4/ - x 4 )</span></p>
+                <p>Pizza 04 <span>( $2/ - x 1 )</span></p>
+            </div>
+            <div class="flex">
+                <form action="" method="post">
+                    <h3>Iniciar sesión</h3>
+                    <input type="email" name="email" required class="box" placeholder="Ingresa tu correo electrónico" maxlength="50">
+                    <input type="password" name="pass" required class="box" placeholder="Ingresa tu contraseña" maxlength="20">
+                    <input type="submit" value="Iniciar sesión" name="login" class="btn">
+                </form>
+                <form action="" method="post">
+                    <h3>Registrarse</h3>
+                    <input type="text" name="name" required class="box" placeholder="Ingresa tu nombre" maxlength="20">
+                    <input type="email" name="email" required class="box" placeholder="Ingresa tu correo electrónico" maxlength="50">
+                    <input type="password" name="pass" required class="box" placeholder="Ingresa tu contraseña" maxlength="20">
+                    <input type="password" name="cpass" required class="box" placeholder="Confirma tu contraseña" maxlength="20">
+                    <input type="submit" value="Registrarse" name="register" class="btn">
+                </form>
+            </div>
+        </section>
+    </div>
+    <!-- Sección de cuenta de usuario termina aquí -->
+
+    <!-- Sección de pedidos comienza aquí -->
+    <div class="my-orders">
+        <section class="container">
+            <div id="close-orders"><span>Cerrar</span></div>
+            <h3 class="title">Mis pedidos</h3>
+            <div class="box">
+                <p>Realizado el: <span>08/10/2022</span></p>
+                <p>Nombre: <span>Jamshid Askarov</span></p>
+                <p>Número: <span>99894-661-16-34</span></p>
+                <p>Dirección: <span>30, 5, 6kv, Distrito Yunusobod, Ciudad de Taskent, Uzb</span></p>
+                <p>Método de pago: <span>Efectivo a la entrega</span></p>
+                <p>Tus pedidos: <span>Pizza 01 $3/- x 2, Pizza 03 $2/- x 1, Pizza 06 $4/- x 4</span></p>
+                <p>Precio total: <span>$24/-</span></p>
+                <p>Estado del pago: <span style="color: var(--red);">Pendiente</span></p>
+            </div>
+            <div class="box">
+                <p>Realizado el: <span>08/10/2022</span></p>
+                <p>Nombre: <span>Jamshid Askarov</span></p>
+                <p>Número: <span>99894-661-16-34</span></p>
+                <p>Dirección: <span>30, 5, 6kv, Distrito Yunusobod, Ciudad de Taskent, Uzb</span></p>
+                <p>Método de pago: <span>Efectivo a la entrega</span></p>
+                <p>Tus pedidos: <span>Pizza 01 $3/- x 2, Pizza 03 $2/- x 1, Pizza 06 $4/- x 4</span></p>
+                <p>Precio total: <span>$24/-</span></p>
+                <p>Estado del pago: <span style="color: var(--red);">Pendiente</span></p>
+            </div>
+        </section>
+    </div>
+    <!-- Sección de pedidos termina aquí -->
+
+    <!-- Sección de carrito de compras comienza aquí -->
+    <div class="shopping-cart">
+        <section class="container">
+            <div id="close-cart"><span>Cerrar</span></div>
+            <div class="box">
+                <a href="" class="fas fa-times"></a>
+                <img src="/images/pizza1.jpeg" alt="01">
+                <div class="content">
+                    <p>Pizza-1 <span>( $3/- x 2 )</span></p>
+                    <form action="" method="post">
+                        <input type="number" name="qty" class="qty" min="1" max="10" value="2">
+                        <button type="submit" class="fas fa-edit" name="update_qty"></button>
+                    </form>
+                </div>
+            </div>
+            <div class="box">
+                <a href="" class="fas fa-times"></a>
+                <img src="/images/pizza2.jpeg" alt="03">
+                <div class="content">
+                    <p>Pizza-3 <span>( $2/- x 1 )</span></p>
+                    <form action="" method="post">
+                        <input type="number" name="qty" class="qty" min="1" max="10" value="1">
+                        <button type="submit" class="fas fa-edit" name="update_qty"></button>
+                    </form>
+                </div>
+            </div>
+            <div class "box">
+                <a href="" class="fas fa-times"></a>
+                <img src="/images/pizza.jpeg" alt="06">
+                <div class="content">
+                    <p>Pizza-6 <span>( $4/- x 4 )</span></p>
+                    <form action="" method="post">
+                        <input type="number" name="qty" class="qty" min="1" max="10" value="4">
+                        <button type="submit" class="fas fa-edit" name="update_qty"></button>
+                    </form>
+                </div>
+            </div>
+            <a href="" class="btn">Ordenar ahora</a>
+        </section>
+    </div>
+    <!-- Sección de carrito de compras termina aquí -->
+
+    <!-- Sección de inicio comienza aquí -->
+    <div class="home-bg">
+        <section class="home container" id="home">
+            <div class="slide-container">
+                <div class="slide active">
+
+                    <div class="content">
+                        <h3>Pizza de Pepperoni Casera</h3>
+                        <div class="fas fa-angle-left" onclick="prev()"></div>
+                        <div class="fas fa-angle-right" onclick="next()"></div>
+                    </div>
+                </div>
+                <div class="slide">
+
+                    <div class "content">
+                        <h3>Pizza con Champiñones</h3>
+                        <div class="fas fa-angle-left" onclick="prev()"></div>
+                        <div class="fas fa-angle-right" onclick="next()"></div>
+                    </div>
+                </div>
+                <div class "slide">
+
+                    <!-- <div class="content">
+                        <h3>Mascarpone y Champiñones</h3>
+                        <div class="fas fa-angle-left" onclick="prev()"></div>
+                        <div class="fas fa-angle-right" onclick="next()"></div>
+                    </div> -->
+                </div>
+            </div>
+        </section>
+    </div>
+    <!-- Sección de inicio termina aquí -->
+
     <!-- Sección "Acerca de nosotros" comienza aquí -->
     <section class="about container" id="about">
         <h1 class="heading">Acerca de nosotros</h1>
@@ -81,6 +206,7 @@ $pizzas = obtenerPizzas();
     <!-- Sección "Acerca de nosotros" termina aquí -->
 
     <!-- Sección de menú comienza aquí -->
+    //importante//
     <section class="menu container" id="menu">
         <h1 class="heading">Nuestro menú</h1>
         <div class="box-container">
@@ -107,6 +233,8 @@ $pizzas = obtenerPizzas();
             }
             ?>
         </div>
+
+
     </section>
     <!-- Sección de menú termina aquí -->
 
